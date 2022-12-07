2a0eb4a29a96b6865612ff11a473c7aa8fecff92
kevinharv <kharvey2002@gmail.com>
2022-12-07 09:43:30 -0600

    Changed Electron out for Tauri

--- EmbeddedClient/src/app.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Main from './Main.jsx';
-
-function render() {
-    ReactDOM.render(<Main />, document.body);
-}
-
-render();
