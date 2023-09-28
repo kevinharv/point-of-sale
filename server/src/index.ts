728e0f68fd9434c470ccb75756f8ea151cfc6f1b
kevinharv <kharvey2002@gmail.com>
2023-09-28 18:04:38 -0500

    Setup Fastify; will be going back to Express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,32 @@
-console.log("Hello World");
+import Fastify from 'fastify';
+
+// Instantiate the Fastify server
+const fastify = Fastify({
+  logger: false
+});
+
+
+// Define routes
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+});
+
+
+
+// Start the server
+const start = async () => {
+  try {
+    fastify.listen({ port: 3000, host: '::' }, function (err, address) {
+        if (err) {
+            fastify.log.error(err)
+            process.exit(1)
+        }
+        fastify.log.info(`Server listening on ${address}`)
+    })
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+
+start();
