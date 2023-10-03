6c7b016e777cc5cbc9864ebdb2752a73d4d3626d
kevinharv <kharvey2002@gmail.com>
2023-10-03 18:39:27 -0500

    Remembering how to do route abstraction

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,16 @@
 */
 
 import express from "express";
+import { statusRouter } from "routes/status";
 // import { Prisma, PrismaClient } from "@prisma/client";
 
 const port = 3000;
 // const prisma = new PrismaClient();
 const app = express();
+
+// Import Routes
+app.use('/status', statusRouter);
+
 
 app.get('/', async (req, res) => {
     res.send("Checking if this works! But a I going to automaically reload?");
