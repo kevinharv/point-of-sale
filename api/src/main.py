d0a6ab896abb575cc839fcb5a3db93088f6ef6ac
kevinharv <kharvey2002@gmail.com>
2025-04-03 20:32:29 -0500

    feat: delete everything and start over

---
+++ api/src/main.py
@@ -0,0 +1,7 @@
+from shared.models import Ticket
+
+ticket = Ticket(id=1, name="FooTicket")
+
+
+print("TESTING")
+print(ticket)
---
+++ shared/models.py
@@ -0,0 +1,88 @@
+from pydantic import BaseModel
+
+class Ticket(BaseModel):
+    id: int
+    name: str
+
+    def to_json(self) -> str:
+        return self.model_dump_json()
+
+# === PHYSICAL DOMAIN ===
+
+class Region(BaseModel):
+    id: int
+    name: str
+
+class SiteGroup(BaseModel):
+    id: int
+    name: str
+    region_id: str
+
+class Site(BaseModel):
+    id: int
+    name: str
+    address: str
+    site_group_id: int
+
+class TableGroup(BaseModel):
+    id: int
+    name: str
+    site_id: int
+
+class Table(BaseModel):
+    id: int
+    name: str
+    table_group_id: int
+
+
+# === INFRASTRUCTURE SECTION ===
+
+class KDS(BaseModel):
+    id: int
+    name: str
+    hostname: str
+    fqdn: str
+    ip_address: str
+    role: str   # copying this, not sure what for yet
+    site_id: int
+
+class Workstation(BaseModel):
+    id: int
+    name: str
+    hostname: str
+    fqdn: str
+    ip_address: str
+    site_id: str
+
+class PrinterType():
+    name: str
+    paper_size: str
+    is_thermal: bool
+
+class Printer(BaseModel):
+    id: int
+    name: str
+    hostname: str
+    fqdn: str
+    ip_address: str
+    site_id: str
+    printer_type: PrinterType
+
+
+# === BUSINESS SECTION ===
+
+# Ex: cash, card, Apple Pay? Google Pay? Others?
+class PaymentMethod(BaseModel):
+    id: int
+    name: str
+    site_id: int
+
+
+# === TICKET SECTION ===
+
+class Ticket(BaseModel):
+    id: int
+    site_id: int
+    ticket_type: str
+
+# Ticket Types: table, bar, takeout, delivery, event
